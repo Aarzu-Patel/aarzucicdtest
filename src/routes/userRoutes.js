9a0bf2079b7ab01057588c7833bfd48d75043cfd
Dhaval Patel <dhavalpatel@Dhavals-Laptop.local>
2025-03-12 20:12:09 -0400

    first

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/userController");
+
+// Define routes
+router.post("/", UserController.createUser);
+router.get("/", UserController.getUsers);
+router.get("/:id", UserController.getUserById);
+router.put("/:id", UserController.updateUser);
+router.delete("/:id", UserController.deleteUser);
+
+module.exports = router;
